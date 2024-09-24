854d0524d50623d52969f7aa3b95ea3d0b6e3dde
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2024-09-24 12:39:33 +0500

    slight adj

--- src/components/ui/home/helpers/section1/MapComp/ReviewBooking.jsx
+++ src/components/ui/home/helpers/section1/MapComp/ReviewBooking.jsx
@@ -553,6 +553,7 @@ const ReviewBooking = ({ setComponent }) => {
     } catch (error) {
       console.error("An error occurred:", error);
       alert("An unexpected error occurred. Please try again.");
+      alert(`An unexpected error occurred: ${error.message || error}`);
     }
   };
 
